5715faff6ded439dcd8d97c122f83ca395d6a0a6
JonatanGimenez <john.e.gimenez@gmail.com>
2017-07-04 08:50:12 -0300

    Add files via upload

---
+++ recuperatoriosTP/TP3/Archivos/Texto.cs
@@ -0,0 +1,56 @@
+﻿using Excepciones;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+
+        public bool guardar(string archivo, string datos)
+        {
+            try
+            {
+                using (StreamWriter escritor = new StreamWriter(archivo))
+                {
+                    escritor.WriteLine(datos);
+                }
+            }
+            catch (Exception excepcion)
+            {
+                throw new ArchivosException(excepcion);
+            }
+
+            return true;
+        }
+
+        public bool leer(string archivo, out string datos)
+        {
+            StringBuilder SB = new StringBuilder();
+            string linea;
+
+            try
+            {
+                using (StreamReader lector = new StreamReader(archivo))
+                {
+                    while ((linea = lector.ReadLine()) != null)
+                    {
+                        SB.AppendLine(linea);
+                    }
+
+                    datos = SB.ToString();
+                }
+            }
+            catch (Exception excepcion)
+            {
+                throw new ArchivosException(excepcion);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ recuperatoriosTP/TP3/Archivos/Xml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Excepciones;
+using System.IO;
+using Clases_Instanciables;
+
+
+namespace Archivos
+{
+    public class Xml : IArchivo<Jornada>
+    {
+
+        public bool guardar(string archivo, Jornada datos)
+        {
+            XmlSerializer serializador = new XmlSerializer(datos.GetType());
+
+            try
+            {
+                using (StreamWriter escritor = new StreamWriter(archivo))
+                {
+                    serializador.Serialize(escritor, datos);
+                }
+            }
+            catch (Exception excepcion)
+            {
+                throw new ArchivosException(excepcion);
+            }
+
+            return true;
+        }
+
+        public bool leer(string archivo, out Jornada datos)
+        {
+            XmlSerializer serializador = new XmlSerializer(typeof(Jornada));
+
+            try
+            {
+                using (StreamReader lector = new StreamReader(archivo))
+                {
+                    datos = (Jornada)serializador.Deserialize(lector);
+                }
+            }
+            catch (Exception excepcion)
+            {
+                throw new ArchivosException(excepcion);
+            }
+
+            return true;
+        }
+    }
+}
